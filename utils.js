451333102adf6c95baf74d09e50b13769086a1ce
Henry Derbyshire <henry@htderbyshire.com>
2022-03-09 19:56:15 +0000

    added comments to explain winLoseTable in utils

--- utils.js
+++ utils.js
@@ -3,11 +3,19 @@ function computerPlay() {
 }
 
 function hasUserWon(userChoice, computerChoice) {
+  // 0 - Rock, 1 - Paper, 2 - Scissors
+
+  //            Rock   |  Paper  | Scissors
+  // Rock     | [0, 0]   [1, 0]    [2, 0]
+  // Paper    | [0, 1]   [1, 1]    [2, 1]
+  // Scissors | [0, 2]   [1, 2]    [2, 2]
+
   const winLoseTable = [
     [0, 1, -1],
     [1, 0, -1],
     [1, -1, 0],
   ];
+  // 0 - Draw, 1 - User wins, -1 - User loses
   return winLoseTable[userChoice][computerChoice];
 }
 
